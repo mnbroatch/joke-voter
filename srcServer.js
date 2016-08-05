79dbe77e91f50ae7edc88d09d54cb0101f3f03c9
Matthew Broatch <mbroatch@gmail.com>
2016-08-05 16:06:19 -0700

    more heroku

--- srcServer.js
+++ srcServer.js
@@ -3,7 +3,6 @@ var path = require('path');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var webpack = require('webpack');
 var config = require('../webpack.config');
 var mongoose = require('mongoose');
 
@@ -11,18 +10,10 @@ var api = require('./api');
 
 const PORT = 3000;
 const app = express();
-const compiler = webpack(config);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/jokevote', (err) => {
   console.log(err || 'Mongoose connected!');
 });
-
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-}));
-
-app.use(require('webpack-hot-middleware')(compiler));
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -32,7 +23,7 @@ app.use(cookieParser());
 app.use('/api', api);
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 app.listen(PORT, err => {
