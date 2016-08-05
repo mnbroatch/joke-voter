49c1ff6a2c1353822e1911322f7161bf36c058b9
Matthew Broatch <mbroatch@gmail.com>
2016-08-05 16:29:59 -0700

    more heroku

--- src/API.js
+++ src/API.js
@@ -74,36 +74,6 @@ const API = {
         ServerActions.editJoke(response)
       });
   },
-  addNewUser(user) {
-    post('/api/users', user)
-      .done(response => { 
-        ServerActions.receiveOneUser(response) });
-  },
-  getAllUsers() {
-    get('/api/users')
-      .done(response => {
-        ServerActions.getAllUsers(response)});
-  },
-  removeUser(_id) {
-    ajax({ 
-      url: `/api/users/${_id}`,
-      type: 'DELETE',
-    })
-      .done(response => {
-        ServerActions.removeUser(response)
-      });
-  },
-  editUser(editedUser) {
-    ajax({ 
-      url: `/api/users/`,
-      type: 'PUT',
-      data: editedUser,
-    })
-      .done(response => {
-        ServerActions.editUser(response)
-      });
-  },
-
 }
 
 export default API;
