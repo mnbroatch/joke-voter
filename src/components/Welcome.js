b53d121321bc40186f3b71ac40556196a2e9318c
Matthew Broatch <mnbroatch@gmail.com>
2016-09-14 22:05:16 -0700

    title text clickable

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 import classnames from 'classnames';
 import css from '../css/welcome.css';
@@ -7,8 +8,9 @@ export default class Welcome extends React.Component {
   render() {
     return (
       <div className={css.titleContainer}>
-        <div className={css.titleText}>
-        </div>
+        <Link to="jokes">
+          <div className={css.titleText}></div>
+        </Link>
         <div className={css.explanation}>
           Welcome to Heh2Heh, where jokes are pitted against each other for your amusement. Simply vote on the joke you find funnier out of each displayed pair. The weakest are regularly removed and replaced with a fresh set.
           <br /> 
